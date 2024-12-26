60b531872d6567705ed0429808c9aaddea91b520
Nusaibah Ibrahimi <nusaibah.ibr@gmail.com>
2024-12-26 14:13:22 +0100

    Nettoyer le script et compatibilité avec les tables avec bras de trt

--- R/compare_grade.R
+++ R/compare_grade.R
@@ -2,17 +2,17 @@
 # output: if difference, dataframe with each row details of difference
 # if no difference, dataframe displaying table R and table SAS side by side.
 # author: Nusaibah
+
 compare_grade <- function(tabR,tabSAS){
   tab=list()
-  if (nrow(tabR)!=nrow(tabSAS)){#warning("Different number of grade levels")
+
+   if (nrow(tabR)!=nrow(tabSAS)){ #warning("Different number of grade levels")
+
     if (nrow(tabR)<nrow(tabSAS)){
       tab$level=c(tab$level,"Mineur")
       tab$grade=c(tab$grade,tabSAS[which(!(tabSAS$grade%in% tabR$grade)),"grade"])
       tab$table=c(tab$table,"R")
       tab$main=c(tab$main,"Missing grade level")
-
-      # tab$Mineur=c(tab$Mineur,
-      #              paste0("Grade ",tabSAS[!(which(tabSAS$grade%in% tabR$grade)),"grade"], " missing in R table"))
 
     }else if (nrow(tabR)>nrow(tabSAS)){
       tab$level=c(tab$level,"Mineur")
@@ -20,168 +20,103 @@ compare_grade <- function(tabR,tabSAS){
       tab$table=c(tab$table,"SAS")
       tab$main=c(tab$main,"Missing grade level")
 
-      # tab$Mineur=c(tab$Mineur,
-      #              paste0("Grade ",tabR[!(which(tabR$grade%in% tabSAS$grade)),"grade"], " missing in SAS table"))
-
     }
   }
 
-  if (ncol(tabR)!=ncol(tabSAS)){#warning("Different number of arm")
+  if (ncol(tabR)!=ncol(tabSAS)){  #warning("Different number of arm")
+
     if (ncol(tabR)<ncol(tabSAS)){
       tab$level=c(tab$level,"Mineur")
-      tab$grade=c(tab$grade,tabSAS[!(which(tabSAS$grade%in% tabR$grade)),"grade"])
+      tab$arm=c(tab$arm,paste(unique(str_extract(colnames(tabSAS)[which(!(colnames(tabSAS)%in% colnames(tabR)))],"[:digit:]")),collapse = " & "))
       tab$table=c(tab$table,"R")
       tab$main=c(tab$main,"Missing arm")
       tab$valueR=c(tab$valueR,NA)
       tab$valueSAS=c(tab$valueSAS,"Filled")
 
 
-
-      # tab$Mineur=c(tab$Mineur,
-      #              paste0("Grade ",tabSAS[!(which(tabSAS$grade%in% tabR$grade)),"grade"], " missing in R table"))
-
     }else if (ncol(tabR)>ncol(tabSAS)){
-
       tab$level=c(tab$level,"Mineur")
-      tab$grade=c(tab$grade,tabR[!(which(tabR$grade%in% tabSAS$grade)),"grade"])
+      tab$arm=c(tab$arm,paste(unique(str_extract(colnames(tabR)[which(!(colnames(tabR)%in% colnames(tabSAS)))],"[:digit:]")),collapse = " & "))
       tab$table=c(tab$table,"SAS")
       tab$main=c(tab$main,"Missing arm")
       tab$valueR=c(tab$valueR,"Filled")
       tab$valueSAS=c(tab$valueSAS,NA)
 
-      # tab$Mineur=c(tab$Mineur,
-      #              paste0("Grade ",tabR[!(which(tabR$grade%in% tabSAS$grade)),"grade"], " missing in SAS table"))
-
     }}
-  if (all(dim(tabR)==dim(tabSAS)# & tabR$grade==tabSAS$grade
-  )){
-    # warning("Check: same dimension of tables & same grade") # à fusionner avec la condition similaire mais grade different
+  if (all(dim(tabR)==dim(tabSAS))){
+    # warning("Check: same dimension of tables")
+
     df=tabR%>%arrange(grade)%>%full_join(tabSAS,by="grade",suffix = c(".r",".sas"))
 
-    indice=df%>%
-      filter(grade %in% rbind(setdiff(tabR,tabSAS),setdiff(tabSAS,tabR))$grade)
+   indice=which(df[,paste0(tabR%>%select(-grade)%>%colnames(),paste=".r")]!=df[,paste0(tabSAS%>%select(-grade)%>%colnames(),paste=".sas")],
+                  arr.ind=TRUE)  # difference numérique number or percentage
+    indice[,"col"]=indice[,"col"]+1 # parce qu'on avait retiré le grade
 
-    indice2=which(df[,paste0(tabR%>%select(-grade)%>%colnames(),paste=".r")]!=df[,paste0(tabSAS%>%select(-grade)%>%colnames(),paste=".sas")],
-                  arr.ind=TRUE)
-    indice2[,"col"]=indice2[,"col"]+1 # parce qu'on avait retiré le grade
-
+    indice=rbind(indice,
+                  which(is.na(df),arr.ind = T)%>%as.data.frame())
+                      # difference NA quand 2 grades ne se correspondent pas dans les 2 tables. je garde NA
     if (nrow(indice)!=0){
-      # print(indice)
-      # warning(paste0("Comparison result: Warning! Different outputs.",
-      #                nrow(indice)," mismatching between the two tables. Above, the indices."))
-      for (i in 1:nrow(indice)){
-        if (is.na(indice[i,"N1.r"])){
+      for (i in 1: nrow(indice)){
 
+if (is.na(df[indice[i,"row"],indice[i,"col"]]) & grepl(".r",colnames(df)[indice[i,"col"]])){
           tab$level=c(tab$level,"Mineur")
-          tab$grade=c(tab$grade,indice[i,"grade"])
+          tab$grade=c(tab$grade,df[indice[i,"row"],"grade"]%>%pull)
           tab$table=c(tab$table,"R")
           tab$main=c(tab$main,"Missing grade level")
           tab$valueR=c(tab$valueR,NA)
-          tab$valueSAS=c(tab$valueSAS,paste0(indice[i,"N1.sas"],"(",indice[i,"pct1.sas"],"%)"))
+          tab$valueSAS=c(tab$valueSAS,
+                         paste0(df[indice[i,"row"],indice[i,"col"]+2],
+                                "(",df[indice[i,"row"],indice[i,"col"]+3],"%)"))
+if (ncol(tabR)>3 & ncol(tabSAS)>3){
+  tab$arm = c(tab$arm,str_extract(string=colnames(df)[indice[i,"col"]],pattern = "[:digit:]"))
+}
 
-
-
-          # tab$Mineur=c(tab$Mineur,
-          #                                       paste0("Missing grade ",indice[i,"grade"]," in table R"))
-        }else if (is.na(indice[i,"N1.sas"])){
+        }else if (is.na(df[indice[i,"row"],indice[i,"col"]]) & grepl(".sas",colnames(df)[indice[i,"col"]])){
           tab$level=c(tab$level,"Mineur")
-          tab$grade=c(tab$grade,indice[i,"grade"])
+          tab$grade=c(tab$grade,df[indice[i,"row"],"grade"]%>%pull)
           tab$table=c(tab$table,"SAS")
           tab$main=c(tab$main,"Missing grade level")
 
-          tab$valueR=c(tab$valueR,paste0(indice[i,"N1.r"],"(",indice[i,"pct1.r"],"%)"))
+          tab$valueR=c(tab$valueR, paste0(df[indice[i,"row"],indice[i,"col"]-2],
+                                          "(",df[indice[i,"row"],indice[i,"col"]-1],"%)"))
           tab$valueSAS=c(tab$valueSAS,NA)
 
-
-
-          # tab$Mineur=c(tab$Mineur,
-          #                                              paste0("Missing grade ",indice[i,"grade"]," in table SAS"))
-        }else if(indice2[i,"col"]%%2==0){
+          if (ncol(tabR)>3 & ncol(tabSAS)>3){
+            tab$arm = c(tab$arm,str_extract(string=colnames(df)[indice[i,"col"]],pattern = "[:digit:]"))
+          }
+        }else if(indice[i,"col"]%%2==0){
 
           tab$level=c(tab$level,"MAJEUR")
-          tab$grade=c(tab$grade,df[indice2[i,"row"],"grade"])
+          tab$grade=c(tab$grade,df[indice[i,"row"],"grade"]%>%pull)
           tab$table=c(tab$table,"Both")
           tab$main=c(tab$main,"Different number")
-          tab$valueR=c(tab$valueR,tabR[indice2[i,"row"],indice2[i,"col"]])
-          tab$valueSAS=c(tab$valueSAS,tabSAS[indice2[i,"row"],indice2[i,"col"]])
-
-
-
-          # tab$MAJEUR=c(tab$MAJEUR,
-          #                                        paste0("Grade ",df[indice[i,"row"],"grade"],
-          #                                               ". Different number:  table SAS N=",tabSAS[indice[i,"row"],indice[i,"col"]], ";  table R N=", tabR[indice[i,"row"],indice[i,"col"]]))
-        }else if (indice2[i,"col"]%%2==1){
+          tab$valueR=c(tab$valueR,tabR[indice[i,"row"],indice[i,"col"]]%>%pull)
+          tab$valueSAS=c(tab$valueSAS,tabSAS[indice[i,"row"],indice[i,"col"]]%>%pull)
+          if (ncol(tabR)>3 & ncol(tabSAS)>3){
+            tab$arm = c(tab$arm,str_extract(string=colnames(df)[indice[i,"col"]],pattern = "[:digit:]"))
+          }
+        }else if (indice[i,"col"]%%2==1){
 
 
           tab$level=c(tab$level,"MAJEUR")
-          tab$grade=c(tab$grade,df[indice2[i,"row"],"grade"])
+          tab$grade=c(tab$grade,df[indice[i,"row"],"grade"]%>%pull)
           tab$table=c(tab$table,"Both")
           tab$main=c(tab$main,"Different percentage")
-          tab$valueR=c(tab$valueR,tabR[indice2[i,"row"],indice2[i,"col"]])
-          tab$valueSAS=c(tab$valueSAS,tabSAS[indice2[i,"row"],indice2[i,"col"]])
-
-
-
-
-          # tab$MAJEUR=c(tab$MAJEUR,
-          #                                              paste0("Grade ",df[indice[i,"row"],"grade"],
-          #                                                     ". Different percentage: table SAS ",tabSAS[indice[i,"row"],indice[i,"col"]], "%; table R ", tabR[indice[i,"row"],indice[i,"col"]],"%."))
+          tab$valueR=c(tab$valueR,tabR[indice[i,"row"],indice[i,"col"]]%>%pull)
+          tab$valueSAS=c(tab$valueSAS,tabSAS[indice[i,"row"],indice[i,"col"]]%>%pull)
+          if (ncol(tabR)>3 & ncol(tabSAS)>3){
+            tab$arm = c(tab$arm,str_extract(string=colnames(df)[indice[i,"col"]],pattern = "[:digit:]"))
+          }
         }
 
 
       }
-    }
-
-
-
-    # else if(all(dim(tabR)==dim(tabSAS)) & !all(tabR$grade==tabSAS$grade)){
-    #
-    #
-    #   warning("Check: same dimension of tables BUT missing grade(s)")
-    #   df=tabR%>%arrange(grade)%>%full_join(tabSAS,by="grade",suffix = c(".r",".sas"))
-    #
-    #
-    #   indice=df%>%
-    #     filter(grade %in% c(setdiff(tabR$grade,tabSAS$grade),setdiff(tabSAS$grade,tabR$grade)))
-    #
-    #
-    #   if (nrow(indice)!=0){
-    #     print(indice)
-    #     warning(paste0("Comparison result: Warning! Different outputs. ",
-    #                    nrow(indice)," mismatching between the two tables. Above, the indices."))
-    #     for (i in 1:nrow(indice)){
-    #
-    #       if (is.na(indice[i,"N1.r"])){
-    #
-    #         tab$level=c(tab$level,"Mineur")
-    #         tab$grade=c(tab$grade,indice[i,"grade"])
-    #         tab$table=c(tab$table,"R")
-    #         tab$main=c(tab$main,"Missing grade levels")
-    #
-    #
-    #         # tab$Mineur=c(tab$Mineur,
-    #         #                                       paste0("Missing grade ",indice[i,"grade"]," in table R"))
-    #         }else if (is.na(indice[i,"N1.sas"])){
-    #         tab$level=c(tab$level,"Mineur")
-    #         tab$grade=c(tab$grade,indice[i,"grade"])
-    #         tab$table=c(tab$table,"SAS")
-    #         tab$main=c(tab$main,"Missing grade levels")
-    #
-    #
-    #
-    #         # tab$Mineur=c(tab$Mineur,
-    #         #                                              paste0("Missing grade ",indice[i,"grade"]," in table SAS"))
-    #       }
-    #
-    #     }
-    #
-    #
-    #   }
-    #
+      tab=as.data.frame(tab)%>%distinct(level,grade,table,main,.keep_all = TRUE)
     }else{  #   warning("Comparison result: same outputs")
-tab=cbind(data.frame("R table"=""),tabR,data.frame("SAS table"=""),tabSAS)
+    tab=cbind(data.frame("R table"=""),tabR,data.frame("SAS table"=""),tabSAS)
+    }
   }
-  tab=as.data.frame(tab)
-  return(tab)
+
+  return(as.data.frame(tab))
 }
 
