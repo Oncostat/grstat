f2216dde6b4e17c3b919ffb7860f883f3126a42f
Dan Chaltiel <15105152+DanChaltiel@users.noreply.github.com>
2025-02-09 11:20:40 +0100

    unequal frequence of AESOC/AETERM
    sample_ctcae in frequence order

--- R/data.R
+++ R/data.R
@@ -108,7 +108,7 @@ example_ae = function(enrolres, p_na=0,
       sae = fct_yesno(runif(n())<ifelse(arm=="Control", p_sae, p_sae_trt)),
       rate = beta0 + beta_trt*(arm!="Control") + beta_sae*(sae=="Yes"),
       aegr = .random_grades_n(rate),
-      .sample_term(n()),
+      .sample_term(n()), #creates `aesoc` and `aeterm`
       across(names(p_na), ~{
         p = p_na[[cur_column()]]
         if_else(runif(n()) < p, NA, .x)
@@ -172,12 +172,16 @@ example_ae = function(enrolres, p_na=0,
 
 
 #' Used in `.example_ae()`
+#' Take `n` items from `sample_ctcae` (length 27), then take one random child of each.
+#' The probability is exponentially decreasing along `names(sample_ctcae)`.
 #' @noRd
 #' @keywords internal
 #' @importFrom purrr map_chr
 #' @importFrom tibble enframe
 .sample_term = function(n){
   sample(sample_ctcae, size=n, replace=TRUE) %>%
+  proba = exp(rev(seq_along(sample_ctcae))/10)
+  sample(sample_ctcae, size=n, replace=TRUE, prob=proba) %>%
     map_chr(~sample(.x, 1)) %>%
     enframe(name="aesoc", value="aeterm")
 }
@@ -191,33 +195,109 @@ causality =c("Experimental treatment", "Standard treatment",
 #Courtesy to ChatGPT.
 #Prompt: "give me an R list with 4 examples of HLGT per SOC"
 sample_ctcae = list(
-  "Blood and lymphatic system disorders" = c("Hematologic neoplasms", "Bone marrow disorders", "Coagulation and bleeding analyses", "Red blood cell disorders"),
-  "Cardiac disorders" = c("Cardiac arrhythmias", "Cardiac valve disorders", "Coronary artery disorders", "Heart failures"),
-  "Congenital, familial and genetic disorders" = c("Chromosomal abnormalities", "Hereditary connective tissue disorders", "Familial hematologic disorders", "Congenital nervous system disorders"),
-  "Ear and labyrinth disorders" = c("Hearing disorders", "Vertigo and balance disorders", "Labyrinth disorders", "Tinnitus"),
-  "Endocrine disorders" = c("Adrenal gland disorders", "Pituitary gland disorders", "Thyroid gland disorders", "Parathyroid gland disorders"),
-  "Eye disorders" = c("Vision disorders", "Eyelid disorders", "Retinal disorders", "Corneal disorders"),
-  "Gastrointestinal disorders" = c("Gastrointestinal motility and defecation conditions", "Esophageal disorders", "Gastric disorders", "Intestinal disorders"),
-  "General disorders and administration site conditions" = c("Device issues", "Pain and discomfort", "General physical health deterioration", "Injection site reactions"),
-  "Hepatobiliary disorders" = c("Liver disorders", "Bile duct disorders", "Gallbladder disorders", "Hepatic failure"),
-  "Immune system disorders" = c("Hypersensitivity conditions", "Immune system analysis disorders", "Immune-mediated adverse events", "Autoimmune diseases"),
-  "Infections and infestations" = c("Bacterial infectious disorders", "Fungal infectious disorders", "Parasitic infectious disorders", "Viral infectious disorders"),
-  "Injury, poisoning and procedural complications" = c("Procedural complications", "Traumatic injuries", "Poisonings", "Radiation-related toxicities"),
-  "Investigations" = c("Blood analyses", "Imaging studies", "Liver function analyses", "Cardiovascular assessments"),
-  "Metabolism and nutrition disorders" = c("Fluid and electrolyte disorders", "Lipid metabolism disorders", "Nutritional disorders", "Vitamin deficiencies"),
-  "Musculoskeletal and connective tissue disorders" = c("Arthritis and joint disorders", "Bone disorders", "Muscle disorders", "Connective tissue disorders"),
-  "Neoplasms benign, malignant, and unspecified" = c("Benign neoplasms", "Malignant neoplasms", "Tumor progression", "Neoplasms unspecified"),
-  "Nervous system disorders" = c("Neurological disorders of the central nervous system", "Headache disorders", "Seizure disorders", "Peripheral neuropathies"),
-  "Psychiatric disorders" = c("Anxiety disorders", "Mood disorders", "Sleep disorders", "Substance-related disorders"),
-  "Renal and urinary disorders" = c("Kidney disorders", "Bladder disorders", "Urethral disorders", "Urinary tract disorders"),
-  "Reproductive system and breast disorders" = c("Female reproductive disorders", "Male reproductive disorders", "Breast disorders", "Menstrual disorders"),
-  "Respiratory, thoracic and mediastinal disorders" = c("Pulmonary vascular disorders", "Respiratory infections", "Pleural disorders", "Lung function disorders"),
-  "Skin and subcutaneous tissue disorders" = c("Skin infections", "Skin pigmentation disorders", "Skin and subcutaneous tissue injuries", "Dermatitis"),
-  "Social circumstances" = c("Social and environmental issues", "Family support issues", "Economic conditions affecting care", "Cultural issues"),
-  "Surgical and medical procedures" = c("Diagnostic procedures", "Therapeutic procedures", "Device implantation procedures", "Surgical complications"),
-  "Vascular disorders" = c("Hypertension-related conditions", "Hypotension-related conditions", "Vascular hemorrhagic disorders", "Venous thromboembolic events"),
-  "Pregnancy, puerperium and perinatal conditions" = c("Pregnancy complications", "Labor and delivery complications", "Fetal complications", "Breastfeeding issues"),
-  "Immune system disorders" = c("Autoimmune disorders", "Alloimmune responses", "Immunodeficiency", "Inflammatory responses")
+  `Gastrointestinal disorders` = c(
+    "Gastrointestinal motility and defecation conditions", "Esophageal disorders",
+    "Gastric disorders", "Intestinal disorders"
+  ),
+  `General disorders and administration site conditions` = c(
+    "Device issues", "Pain and discomfort",
+    "General physical health deterioration", "Injection site reactions"
+  ),
+  `Blood and lymphatic system disorders` = c(
+    "Hematologic neoplasms", "Bone marrow disorders",
+    "Coagulation and bleeding analyses", "Red blood cell disorders"
+  ),
+  `Renal and urinary disorders` = c(
+    "Kidney disorders", "Bladder disorders", "Urethral disorders",
+    "Urinary tract disorders"
+  ),
+  `Reproductive system and breast disorders` = c(
+    "Female reproductive disorders", "Male reproductive disorders",
+    "Breast disorders", "Menstrual disorders"
+  ),
+  Investigations = c(
+    "Blood analyses", "Imaging studies", "Liver function analyses",
+    "Cardiovascular assessments"
+  ),
+  `Infections and infestations` = c(
+    "Bacterial infectious disorders", "Fungal infectious disorders",
+    "Parasitic infectious disorders", "Viral infectious disorders"
+  ),
+  `Metabolism and nutrition disorders` = c(
+    "Fluid and electrolyte disorders", "Lipid metabolism disorders",
+    "Nutritional disorders", "Vitamin deficiencies"
+  ),
+  `Skin and subcutaneous tissue disorders` = c(
+    "Skin infections", "Skin pigmentation disorders",
+    "Skin and subcutaneous tissue injuries", "Dermatitis"
+  ),
+  `Nervous system disorders` = c(
+    "Neurological disorders of the central nervous system", "Headache disorders",
+    "Seizure disorders", "Peripheral neuropathies"
+  ),
+  `Musculoskeletal and connective tissue disorders` = c(
+    "Arthritis and joint disorders", "Bone disorders",
+    "Muscle disorders", "Connective tissue disorders"
+  ),
+  `Ear and labyrinth disorders` = c(
+    "Hearing disorders", "Vertigo and balance disorders",
+    "Labyrinth disorders", "Tinnitus"
+  ),
+  `Vascular disorders` = c(
+    "Hypertension-related conditions", "Hypotension-related conditions",
+    "Vascular hemorrhagic disorders", "Venous thromboembolic events"
+  ),
+  `Endocrine disorders` = c(
+    "Adrenal gland disorders", "Pituitary gland disorders",
+    "Thyroid gland disorders", "Parathyroid gland disorders"
+  ),
+  `Psychiatric disorders` = c(
+    "Anxiety disorders", "Mood disorders",
+    "Sleep disorders", "Substance-related disorders"
+  ),
+  `Respiratory, thoracic and mediastinal disorders` = c(
+    "Pulmonary vascular disorders", "Respiratory infections",
+    "Pleural disorders", "Lung function disorders"
+  ),
+  `Hepatobiliary disorders` = c(
+    "Liver disorders", "Bile duct disorders",
+    "Gallbladder disorders", "Hepatic failure"
+  ),
+  `Cardiac disorders` = c(
+    "Cardiac arrhythmias", "Cardiac valve disorders",
+    "Coronary artery disorders", "Heart failures"
+  ),
+  `Immune system disorders` = c(
+    "Autoimmune disorders", "Hypersensitivity conditions",
+    "Immunodeficiency", "Inflammatory responses"
+  ),
+  `Injury, poisoning and procedural complications` = c(
+    "Procedural complications", "Traumatic injuries",
+    "Poisonings", "Radiation-related toxicities"
+  ),
+  `Neoplasms benign, malignant, and unspecified` = c(
+    "Benign neoplasms", "Malignant neoplasms",
+    "Tumor progression", "Neoplasms unspecified"
+  ),
+  `Surgical and medical procedures` = c(
+    "Diagnostic procedures", "Therapeutic procedures",
+    "Device implantation procedures", "Surgical complications"
+  ),
+  `Eye disorders` = c(
+    "Vision disorders", "Eyelid disorders",
+    "Retinal disorders", "Corneal disorders"),
+  `Pregnancy, puerperium and perinatal conditions` = c(
+    "Pregnancy complications", "Labor and delivery complications",
+    "Fetal complications", "Breastfeeding issues"
+  ),
+  `Social circumstances` = c(
+    "Social and environmental issues", "Family support issues",
+    "Economic conditions affecting care", "Cultural issues"
+  ),
+  `Congenital, familial and genetic disorders` = c(
+    "Chromosomal abnormalities", "Hereditary connective tissue disorders",
+    "Familial hematologic disorders", "Congenital nervous system disorders"
+  )
 )
 
 stopifnot(unique(lengths(sample_ctcae)) == 4)
