9719afb4b783952aaffe10990d8a41cab8a9a3fe
Dan Chaltiel <15105152+DanChaltiel@users.noreply.github.com>
2025-02-09 14:23:19 +0100

    update data test with exponential grade decay

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -1,11 +1,22 @@
 
 
 test_that("Proportions of AE grades are respected", {
-  p_ae  = c(0.4,0.3,0.2,0.1,0.01)
-  p_sae = c(0.15,0.15,0.3,0.3,0.1)
-  tm = grstat_example(2000, ae_p_na=0, ae_p_sae=0.5, seed=42)
+  skip_on_ci() #random number generation
+
+  get_rate = function(sae){
+    rate = -1+sae
+    probs = exp(rate * c(1:4))
+    probs = c(probs, probs[4]/7) #grade 5 always minor
+    probs / sum(probs)
+  }
+  p_ae  = get_rate(FALSE)
+  p_sae = get_rate(TRUE)
+
+
+  tm = grstat_example(5000, p_na=0, p_sae=0.5, beta_trt=0, seed=42)
   x = tm$ae %>% with(table(aegr, sae)) %>% prop.table(margin=2) %>% round(2)
-  # as.matrix(x)/cbind(p_sae, p_ae)
+  round(as.matrix(x)/cbind(p_sae, p_ae), 1)
+
   expect_equal(unname(as.matrix(x)[,"Yes"]), p_sae, tolerance=0.05) # 5% tolerance
-  expect_equal(unname(as.matrix(x)[,"No"]),   p_ae, tolerance=0.05)
+  expect_equal(unname(as.matrix(x)[,"No"]),  p_ae,  tolerance=0.05)
 })
