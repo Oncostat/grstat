046f38b2a59d07c703d78d91aca134e5c68aa0f4
Dan Chaltiel <15105152+DanChaltiel@users.noreply.github.com>
2025-06-13 11:40:34 +0200

    improve globalVariables() management

--- R/grstat-package.R
+++ R/grstat-package.R
@@ -25,22 +25,24 @@ flextable::as_flextable
 #helper: copy-paste the CHECK output in a string, apply f(), and append to globalVariables()
 f = function(x) x %>% stringr::str_squish() %>% str_replace_all("[ \n]", '", "') %>% cat('"', ., '"', sep="")
 
-utils::globalVariables(
-  c(".", ".data", ".env", ".id", ".name", "aegr", "aegr_sae", "aerel",
-    "aesoc", "aeterm", "any_ae", "any_grade_sup_na", "any_severe",
-    "arm", "arm_", "baseline", "baseline_rc_date", "calc", "code",
-    "col_keys", "diff_abs_nad", "diff_first", "diff_rel_bl", "diff_rel_nad",
-    "first_date", "first_sum", "global_resp", "global_resp_check",
-    "global_resp_num", "grade_", "h1", "h2", "label", "level", "min_sum",
-    "n_ae", "n_arm", "n_severe", "n_sites", "n_subjid", "name", "new_lesion",
-    "new_lesions", "non_measurable_lesion", "non_measurable_node",
-    "nontarget_resp", "nontarget_resp_num", "nontarget_yn", "pct_ae",
-    "pct_severe", "post_pd", "rate", "rc_date", "remaining_lesion",
-    "remaining_node", "resp", "resp_num", "resp2", "response_value",
-    "sae", "severe_", "soc_", "soc_weight", "subjid", "subjid_",
-    "sum_bl", "sum_nadir", "target_diam", "target_is_node", "target_method",
-    "target_resp", "target_resp_num", "target_site", "target_sum",
-    "target_sum_bl", "target_sum_bl_real", "target_sum_min", "target_sum_real",
-    "Tot", "value", "variable", "weight", "x")
+globals = c(
+  ".", ".data", ".env", ".id", ".name", "aegr", "aegr_sae", "aerel",
+  "aesoc", "aeterm", "any_ae", "any_grade_sup_na", "any_severe",
+  "arm", "arm_", "baseline", "baseline_rc_date", "calc", "code",
+  "col_keys", "diff_abs_nad", "diff_first", "diff_rel_bl", "diff_rel_nad",
+  "first_cr", "first_date", "first_pd", "first_sum", "global_resp",
+  "global_resp_check", "global_resp_num", "grade_", "h1", "h2",
+  "label", "level", "min_sum", "n_ae", "n_arm", "n_severe", "n_sites",
+  "n_subjid", "name", "new_lesion", "new_lesions", "non_measurable_lesion",
+  "non_measurable_node", "nontarget_resp", "nontarget_resp_num",
+  "nontarget_yn", "pct_ae", "pct_severe", "post_pd", "rate", "rc_date",
+  "real_target_sum_min", "remaining_lesion", "remaining_node",
+  "resp", "resp_num", "resp2", "response_num", "response_value",
+  "sae", "severe_", "soc_", "soc_weight", "subjid", "subjid_",
+  "sum_bl", "sum_nadir", "target_diam", "target_diam_sum", "target_is_node",
+  "target_method", "target_resp", "target_resp_num", "target_site",
+  "target_sum", "target_sum_bl", "target_sum_bl_real", "target_sum_min",
+  "target_sum_real", "Tot", "value", "variable", "weight", "x"
 )
-
+# dput(sort(globals))
+utils::globalVariables(globals)
