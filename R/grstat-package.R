5ff384d17dbf0e120cce1b61cc76163ca48a29ad
Dan Chaltiel <15105152+DanChaltiel@users.noreply.github.com>
2025-06-13 14:59:39 +0200

    improve globalVariables management

--- R/grstat-package.R
+++ R/grstat-package.R
@@ -25,14 +25,30 @@ flextable::as_flextable
 #helper: copy-paste the CHECK output in a string, apply f(), and append to globalVariables()
 f = function(x) x %>% stringr::str_squish() %>% str_replace_all("[ \n]", '", "') %>% cat('"', ., '"', sep="")
 
-utils::globalVariables(
-  c(".", ".data", ".env", ".id", ".name", "Tot", "aegr", "aesoc", "any_ae", "any_grade_sup_na",
-    "any_severe", "arm_", "calc", "col_keys", "diff_first", "first_date", "arm", "rate", "aerel",
-    "first_sum", "grade_", "h1", "h2", "label", "min_sum", "aegr_sae", "aeterm", "soc_weight",
-    "n_ae", "n_arm", "n_severe", "name", "pct_ae", "pct_severe", "resp", "resp2", "resp_num",
-    "sae", "severe_", "soc_", "subjid", "subjid_", "value", "variable", "weight", "x",
-    "num_timepoints", "rc_num_timepoints", "rctlsum_b", "rc_sd_tlsum_noise", "delai",
-    "percent_change_per_month", "changes", "base", "sizes", "rctlsum", "percent_change",
-    "rctlmin", "rcvisit", "rcdt", "rctlresp", "rcntlresp", "rcnew", "rc_p_new_lesions", "not_evaluable",
-    "rc_p_not_evaluable", "rcresp", "suivi", "arm3", "date_inclusion", "rc_p_nt_lesions", "data","rc_coef_treatement")
+globals = c(
+  ".", ".data", ".env", ".id", ".name", "aegr", "aegr_sae", "aerel",
+  "aesoc", "aeterm", "any_ae", "any_grade_sup_na", "any_severe",
+  "arm", "arm_", "arm3", "base", "baseline", "baseline_rc_date",
+  "best_response", "calc", "changes", "code", "col_keys", "color",
+  "data", "date_inclusion", "delai", "diff_abs_nad", "diff_first",
+  "diff_min", "diff_rel_bl", "diff_rel_nad", "first_cr", "first_date",
+  "first_pd", "first_sum", "global_resp", "global_resp_check",
+  "global_resp_num", "grade_", "h1", "h2", "label", "level", "min_sum",
+  "n_ae", "n_arm", "n_severe", "n_sites", "n_subjid", "name", "new_lesion",
+  "new_lesions", "non_measurable_lesion", "non_measurable_node",
+  "nontarget_resp", "nontarget_resp_num", "nontarget_yn", "not_evaluable",
+  "num_timepoints", "pct_ae", "pct_severe", "percent_change", "percent_change_per_month",
+  "post_pd", "rate", "rc_coef_treatement", "rc_date", "rc_num_timepoints",
+  "rc_p_new_lesions", "rc_p_not_evaluable", "rc_p_nt_lesions",
+  "rc_sd_tlsum_noise", "rcdt", "rcnew", "rcntlresp", "rcresp",
+  "rctlmin", "rctlresp", "rctlsum", "rctlsum_b", "rcvisit", "real_target_sum_min",
+  "remaining_lesion", "remaining_node", "resp", "resp_num", "resp2",
+  "response", "response_num", "response_value", "sae", "severe_",
+  "sizes", "soc_", "soc_weight", "subjid", "subjid_", "suivi",
+  "sum_bl", "sum_nadir", "target_diam", "target_diam_sum", "target_is_node",
+  "target_method", "target_resp", "target_resp_num", "target_site",
+  "target_sum", "target_sum_bl", "target_sum_bl_real", "target_sum_min",
+  "target_sum_real", "Tot", "value", "variable", "weight", "x", "y"
 )
+# dput(sort(unique(globals)))
+utils::globalVariables(globals)
