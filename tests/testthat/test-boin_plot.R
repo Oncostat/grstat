bd11b9ff60be763bb983dfabd2cb17c708205ce2
Dan Chaltiel <15105152+DanChaltiel@users.noreply.github.com>
2025-07-07 21:42:27 +0200

    add tests

---
+++ tests/testthat/test-boin_plot.R
@@ -0,0 +1,54 @@
+test_that("boin_plot works", {
+
+  boin = BOIN::get.boundary(target=0.3, ncohort=1, cohortsize=15)
+  set.seed(42)
+  data_patients = tibble(
+    subjid = 1:17,
+    dose = c("DL0", "DL0", "DL-1", "DL-1", "DL-1", "DL0", "DL0", "DL0",
+             "DL1", "DL1", "DL1", "DL1", "DL1", "DL1", "DL1", "DL1", "DL1"),
+    dlt = c(FALSE, TRUE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE,
+            FALSE, FALSE, FALSE, FALSE, TRUE, FALSE, FALSE, NA, NA),
+    date_enrol = as.Date("2025-01-01") +(1:17)*30 + runif(17, -13, 13),
+    date_end_fu = date_enrol+30
+  )
+
+  #default
+  p1 = boin_plot(boin,
+            doses=c("DL-1", "DL0", "DL1", "DL2"))
+  vdiffr::expect_doppelganger("boin-default", p1)
+
+  #with patient labels
+  p2 = boin_plot(boin, data_patients=data_patients,
+            doses=c("DL-1", "DL0", "DL1", "DL2"))
+  vdiffr::expect_doppelganger("boin-patients", p2)
+  #with gantt diagram
+  p3 = boin_plot(boin, data_patients=data_patients, gantt_include=TRUE,
+                doses=c("DL-1", "DL0", "DL1", "DL2"))
+  vdiffr::expect_doppelganger("boin-gantt", p3)
+
+  #with post production
+  p3[[1]] = p3[[1]] + scale_color_manual(values=c("red", "green", "blue", "yellow"))
+  p3[[2]] = p3[[2]] + scale_color_manual(values=c("red", "green", "blue"))
+  vdiffr::expect_doppelganger("boin-gantt-modif", p3)
+
+  #with gantt labels
+  gantt_labels=c("Plot cutoff date"=as.Date("2026-08-25"),
+                 "Dose reevaluation"=as.Date("2025-08-25"))
+  p4 = boin_plot(data_boin=boin, data_patients=data_patients,
+            doses = c("DL-1", "DL0", "DL1", "DL2"),
+            gantt_include=TRUE,
+            gantt_labels=gantt_labels)
+  vdiffr::expect_doppelganger("boin-gantt-labels", p4)
+
+
+  #errors
+  boin_plot(boin, data_patients=NULL, gantt_include=TRUE,
+            doses=c("DL-1", "DL0", "DL1", "DL2"))
+  boin_plot(boin, data_patients=data_patients %>% select(1), gantt_include=FALSE,
+            doses=c("DL-1", "DL0", "DL1", "DL2")) %>%
+    expect_error(class="grstat_name_notfound_error", regexp="dose.*dlt")
+  boin_plot(boin, data_patients=data_patients %>% select(1), gantt_include=TRUE,
+            doses=c("DL-1", "DL0", "DL1", "DL2")) %>%
+    expect_error(class="grstat_name_notfound_error", regexp="date_end_fu")
+
+})
