05b7e51bf2d4106a24e8e46189fc25ae61c49978
Dan Chaltiel <dan.chaltiel@gmail.com>
2025-07-09 11:11:56 +0200

    Improve recist data (#75)
    * better arm repartition
    
    * improve date_inclusion calculation
    
    * improve documentation
    
    * example_rc: removed rctlsum_b & rctlmin
    
    * moved test-alexis.R to test-data.R
    
    * example_rc: add NTL yesno column `rcntlyn`
    
    NTL are NE mostly when non present
    
    * example_rc: fixed labels
    
    bind_rows removed them, added at last
    
    * improved documentation
    
    * example_rc: reformat
    
    * add .sample_yesno()
    
    * example_rc: improve workflow
    
    * example_rc: fix number of timepoints
    
    baseline was added
    
    * example_rc: removed unused variables
    
    * example_rc: minor reformat, renaming, and doc
    
    * example_rc: internalise things inside .simulate_one_patient()
    
    rctlsum_b + calculate rcdt from delai
    
    example_rc: simulate baseline inside .simulate_one_patient()
    
    also, decrease linearly (% of baseline) instead of exponentially (% of previous date) for simplicity
    
    * example_rc: revert baseline being not missing
    
    +response factor level management
    
    * example_rc: tweak default values
    
    * add tests on RECIST data (survival rates...)
    
    * add dev tests on RECIST (skipped, for viz only)
    
    * fix for check
    
    * add missing values
    
    * update doc
    
    * add NA/NE for real this time
    
    * update test for NE values
    
    ---------
    
    Co-authored-by: Dan Chaltiel <15105152+DanChaltiel@users.noreply.github.com>

--- R/grstat-package.R
+++ R/grstat-package.R
@@ -25,14 +25,21 @@ flextable::as_flextable
 #helper: copy-paste the CHECK output in a string, apply f(), and append to globalVariables()
 f = function(x) x %>% stringr::str_squish() %>% str_replace_all("[ \n]", '", "') %>% cat('"', ., '"', sep="")
 
-utils::globalVariables(
-  c(".", ".data", ".env", ".id", ".name", "Tot", "aegr", "aesoc", "any_ae", "any_grade_sup_na",
-    "any_severe", "arm_", "calc", "col_keys", "diff_first", "first_date", "arm", "rate", "aerel",
-    "first_sum", "grade_", "h1", "h2", "label", "min_sum", "aegr_sae", "aeterm", "soc_weight",
-    "n_ae", "n_arm", "n_severe", "name", "pct_ae", "pct_severe", "resp", "resp2", "resp_num",
-    "sae", "severe_", "soc_", "subjid", "subjid_", "value", "variable", "weight", "x",
-    "num_timepoints", "rc_num_timepoints", "rctlsum_b", "rc_sd_tlsum_noise", "delai",
-    "percent_change_per_month", "changes", "base", "sizes", "rctlsum", "percent_change",
-    "rctlmin", "rcvisit", "rcdt", "rctlresp", "rcntlresp", "rcnew", "rc_p_new_lesions", "not_evaluable",
-    "rc_p_not_evaluable", "rcresp", "suivi", "arm3", "date_inclusion", "rc_p_nt_lesions", "data","rc_coef_treatement")
+globals = c(
+  ".", ".data", ".env", ".id", ".name", "aegr", "aegr_sae", "aerel",
+  "aesoc", "aeterm", "any_ae", "any_grade_sup_na", "any_severe",
+  "arm", "arm_", "arm3", "base", "before_pd", "calc", "changes",
+  "col_keys", "data", "date_inclusion", "delai", "diff_first",
+  "first_date", "first_sum", "grade_", "h1", "h2", "label", "min_sum",
+  "n_ae", "n_arm", "n_severe", "name", "not_evaluable", "num_timepoints",
+  "pct_ae", "pct_severe", "percent_change", "percent_change_per_month",
+  "rate", "rc_coef_treatement", "rc_num_timepoints", "rc_p_new_lesions",
+  "rc_p_not_evaluable", "rc_p_nt_lesions", "rc_sd_tlsum_noise",
+  "rcdt", "rcnew", "rcntlresp", "rcntlyn", "rcresp", "rctlmin",
+  "rctlresp", "rctlsum", "rctlsum_b", "rcvisit", "resp", "resp_num",
+  "resp2", "sae", "severe_", "sizes", "soc_", "soc_weight", "subj_delai",
+  "subjid", "subjid_", "suivi", "Tot", "value", "variable", "weight",
+  "x"
 )
+# dput(sort(globals))
+utils::globalVariables(globals)
--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -20,3 +20,123 @@ test_that("Proportions of AE grades are respected", {
   expect_equal(unname(as.matrix(x)[,"Yes"]), p_sae, tolerance=0.05) # 5% tolerance
   expect_equal(unname(as.matrix(x)[,"No"]),  p_ae,  tolerance=0.05)
 })
+
+
+test_that("RECIST data are ok", {
+
+  db = grstat_example(N=500)
+  rc = db$recist
+
+  expect_false(any(rc$rcresp=="ERROR", na.rm=TRUE))
+  expect_false(any(rc$rctlsum < 0, na.rm=TRUE))
+
+  is_na_ne = function(x) is.na(x) | x=="Not Evaluable"
+  #baseline vide
+  rc %>%
+    filter(rcdt==min(rcdt), .by=subjid) %>%
+    filter(!is_na_ne(rcresp) | !is_na_ne(rctlresp) | !is_na_ne(rcntlresp) | !is_na_ne(rcnew)) %>%
+    nrow() %>%
+    expect_equal(0)
+
+  #pas de date négative
+  rc %>%
+    left_join(db$enrolres, by="subjid", suffix=c("_rc", "")) %>%
+    select(subjid, date_inclusion, rcdt, everything()) %>%
+    filter(rcdt<date_inclusion) %>%
+    nrow() %>%
+    expect_equal(0)
+
+  #15% more CR, 15% less PD at all time in treatment arm
+  data_resp = rc %>%
+    summarise(
+      any_cr_target=any(rctlresp=="Complete response", na.rm=TRUE),
+      any_cr_global=any(rcresp=="Complete response", na.rm=TRUE),
+      any_pd_target=any(rctlresp=="Progressive disease", na.rm=TRUE),
+      any_pd_global=any(rcresp=="Progressive disease", na.rm=TRUE),
+      .by=subjid
+    ) %>%
+    left_join(db$enrolres, by="subjid", suffix=c("_bak", "")) %>%
+    summarise(across(starts_with("any_"), mean), .by=arm) %>%
+    pivot_longer(-arm) %>%
+    tidyr::pivot_wider(names_from=arm) %>%
+    mutate(diff=Treatment-Control) %>%
+    select(name, diff) %>%
+    deframe()
+  expect_true(all(data_resp[c("any_cr_target", "any_cr_global")] > 0.15, na.rm=TRUE))
+  expect_true(all(data_resp[c("any_pd_target", "any_pd_global")] < -0.15, na.rm=TRUE))
+
+  #PFS is better in treatment arm: HR~0.5, 4yPFS difference > 0.2
+  #TODO add death data
+  data_km = rc %>%
+    summarise(
+      date_pd = min_narm(rcdt[rcresp=="Progressive disease"]),
+      last_date = max_narm(rcdt),
+      .by=subjid
+    ) %>%
+    left_join(db$enrolres, by="subjid", suffix=c("_rc", "")) %>%
+    mutate(
+      date_pfs = pmin(date_pd, last_date, na.rm=TRUE),
+      time_pfs = as.numeric(date_pfs - date_inclusion)/30.4,
+      event_pfs = !is.na(date_pd)
+    )
+  expect_true(all(data_km$time_pfs > 0))
+
+  cox = survival::coxph(survival::Surv(time_pfs, event_pfs) ~ arm, data=data_km)
+  expect_true(coef(cox) < - 0.7)
+
+  km = survival::survfit(survival::Surv(time_pfs, event_pfs) ~ arm, data=data_km)
+  surv_diff_arm_5m = summary(km, times = 4)$surv %>% diff()
+  expect_true(surv_diff_arm_5m > 0.2)
+
+})
+
+
+test_that("RECIST plots", {
+  skip("Skip RECIST plots, kept for dev vizualisation")
+
+  db = grstat_example(N=500)
+  rc = db$recist %>%
+    left_join(x$enrolres, by="subjid", suffix=c("_bak", ""))
+
+  # KM plot
+  km = survival::survfit(survival::Surv(time_pfs, event_pfs) ~ arm, data=data_km)
+  ggsurvfit::ggsurvfit(km) +
+    ggsurvfit::add_censor_mark() +
+    ggsurvfit::add_confidence_interval()
+
+
+  # spaghetti plot from baseline
+  rc %>%
+    mutate(t = as.numeric(rcdt-date_inclusion)/30.5) %>%
+    mutate(rctlsum_chg = (rctlsum-first(rctlsum, order_by=rcdt))/rctlsum, .by=subjid) %>%
+    filter(!is.na(t)) %>%
+    ggplot() +
+    aes(x=t, y=rctlsum_chg, color=arm, group=subjid, rcresp=rcresp, rcvisit=rcvisit) +
+    geom_point(alpha=0.2) +
+    geom_line(alpha=0.2)
+
+  # TL response repartition plot
+  rc %>%
+    filter(rcvisit>1) %>%
+    pivot_longer(c(rctlresp)) %>%
+    # pivot_longer(c(rctlresp, rcntlresp, rcnew, rcresp)) %>%
+    ggplot(aes(x=value, fill=arm)) +
+    geom_bar(position="dodge") +
+    facet_wrap(~name, scale="free") +
+    theme(axis.text.x=element_text(angle=45, hjust=1, vjust=1))
+
+  # TL response repartition crosstable
+  rc %>%
+    filter(rcvisit>1) %>%
+    # filter(rcvisit==max(rcvisit), .by=subjid) %>%
+    mutate(rcvisit=factor(rcvisit)) %>%
+    crosstable(c(ends_with("resp"), rcnew), by=arm, margin=2) %>%
+    # crosstable(c(rcvisit), by=rctlresp, margin=2) %>%
+    # crosstable(c(ends_with("resp"), rcnew), margin=2) %>%
+    af(T, header_show_n=T) %>%
+    print()
+})
+
+
+
+
