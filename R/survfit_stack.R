62ce9dde04b2b6a847013d22d91bf2c6a26b6437
Dan Chaltiel <dan.chaltiel@gmail.com>
2025-07-22 14:49:06 +0200

    doc

--- R/survfit_stack.R
+++ R/survfit_stack.R
@@ -1,3 +1,52 @@
+#' Stack multiple survival endpoints into a single `survfit2` object
+#'
+#' This function combines several survival endpoints from the same dataset by stacking them
+#' into a long format and fitting a single `survfit` object.
+#'
+#' @param data A data frame containing the survival variables.
+#' @param surv_list A named list where each element is a character vector of:
+#'
+#' - Length 2: `time`, `event` (for standard right-censored survival)
+#' - Length 3: `time1`, `time2`, `event` (for interval-censored survival)
+#'
+#' The names of the list elements will be used as a grouping variable in the output.
+#'
+#' @return A `survfit` object (or `survfit2` if `ggsurvfit` is installed)
+#'
+#' @importFrom rlang check_installed is_named
+#' @importFrom cli cli_abort
+#' @export
+#'
+#' @examples
+#' set.seed(42)
+#' df_surv = survival::lung %>%
+#'   as_tibble() %>%
+#'   mutate(
+#'     time_os = time,
+#'     event_os = status == 2,
+#'     time_pfs = time / (1 + runif(n())),
+#'     event_pfs = ifelse(runif(n()) > 0.3, event_os, 1),
+#'     time_efs = time_pfs / (1 + runif(n())),
+#'     event_efs = ifelse(runif(n()) > 0.3, event_pfs, 1)
+#'   )
+#'
+#' surv_list = list(
+#'   "Overall survival" = c("time_os", "event_os"),
+#'   "Progression-free survival" = c("time_pfs", "event_pfs"),
+#'   "Event-free survival" = c("time_efs", "event_efs")
+#' )
+#'
+#' s = survfit_stack(df_surv, surv_list=surv_list)
+#' s
+#'
+#' if(require("ggsurvfit")){
+#'   s %>%
+#'     ggsurvfit() +
+#'     add_confidence_interval() +
+#'     add_risktable() +
+#'     scale_ggsurvfit()
+#'
+#' }
 survfit_stack = function(data, surv_list){
   check_installed("survival", "for `survfit_stack()` to work")
   assert(is_named(surv_list))
