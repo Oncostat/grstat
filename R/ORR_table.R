0b8d1e66c8ae0fcbcfa414ab4c7fdde448cb0899
AlexisCochard <146321117+AlexisCochard@users.noreply.github.com>
2025-09-16 15:54:19 +0200

    maj de orr_table suite aux commentaires

--- R/ORR_table.R
+++ R/ORR_table.R
@@ -1,7 +1,7 @@
 #' Calculate ORR
 #'
 #' @description
-#' The function `ORR_table()` creates a summary table of recist for each possible response.
+#' The function `aggregate_recist_rates()` creates a summary table of recist for each possible response.
 #' The resulting dataframe can be piped to `as_flextable()` to get a nicely formatted flextable.
 #'
 #' @param data A dataset containing longitudinal RECIST data in long format.
@@ -13,7 +13,7 @@
 #' @param cycle_length Numeric, Time between two cycle (used for confirmation), default = 28 days following PharmaSUG 2023 – Paper QT047 recommendation
 #' @param show_CBR Logical; if `TRUE`, show the Clinical Best Response (CBR). CBR was defined as the presence of at least a partial response (PR), complete response (CR), or stable disease (SD) lasting at least six months (using a window of +/-1 month for the RECIST date).
 #'
-#' @return a dataframe (`ORR_table()`) or a flextable (`as_flextable()`).
+#' @return a dataframe (`aggregate_recist_rates()`) or a flextable (`as_flextable()`).
 #'
 #' @importFrom dplyr select any_of all_of
 #' @importFrom cli cli_abort
@@ -24,127 +24,115 @@
 #' df = grstat_example()
 #' recist = df$recist
 #'
-#' ORR_table(recist)
+#' aggregate_recist_rates(recist)
 #'
-#' #the resulting data.frame can be customized using the flextable package
-#' library(flextable)
-#' ORR_table(recist) %>%
+#' #the resulting data.frame can be customized using as_flextable()
+#' aggregate_recist_rates(recist) %>%
 #'   as_flextable()
 #' #Is also possible to use the confirmation method for the ORR
-#' ORR_table(recist, confirmed = TRUE) %>%
+#' aggregate_recist_rates(recist, confirmed = TRUE) %>%
 #'   as_flextable()
 #'
 #' #Or show the Clinical Benefice Rate
-#' ORR_table(recist, show_CBR = TRUE) %>%
+#' aggregate_recist_rates(recist, show_CBR = TRUE) %>%
 #'   as_flextable()
-ORR_table = function(data, ..., rc_resp="RCRESP", rc_date="RCDT",
+aggregate_recist_rates = function(data, ..., rc_resp="RCRESP", rc_date="RCDT",
                      subjid="SUBJID", confirmed = FALSE, show_CBR = FALSE, cycle_length =28){
   recist = calc_best_response(data, rc_resp=rc_resp, rc_date=rc_date,
                               subjid=subjid, confirmed = confirmed, cycle_length = cycle_length)
 
+  if(length(unique(recist$subjid)) != length(recist$subjid)){
+    cli_abort(c("data should be in wide format relative to subjid",
+                i="Please check that there is no duplicate"))
+  }
+
+
   total = length(recist$subjid)
   response_counts = recist %>%
-    crosstable(best_response, percent_pattern = "{n}") %>%
-    rename(Name = variable, N = value) %>%
-    mutate(N = as.numeric(N),
-           Percentage = round(N / sum(N) * 100, 1)) %>%
-    select(Name, N, Percentage)
+    count(best_response, .drop=FALSE) %>%
+    mutate(p=round(n / sum(n) * 100, 1))
 
   ORR = recist %>%
-    filter(Overall_Response==1) %>%
-    summarise(Name = "Overall ORR", N = n()) %>%
-    mutate(Percentage = round(N / total * 100, 1))
+    filter(overall_response==1) %>%
+    summarise(best_response = "Overall Response Rate (ORR)", n = n()) %>%
+    mutate(p = round(n / total * 100, 1))
 
-  CBR = recist %>%
-    filter(Clinical_Benefit==1) %>%
-    summarise(Name = "Clinical Benefit Rate (CBR)", N = n()) %>%
-    mutate(Percentage = round(N / total * 100, 1))
-
+  CBR = NULL
   if(show_CBR){
+    CBR = recist %>%
+      filter(clinical_benefit==1) %>%
+      summarise(best_response = "Clinical Benefit Rate (CBR)", n = n()) %>%
+      mutate(p = round(n / total * 100, 1))
+  }
   summary_df = bind_rows(ORR, response_counts, CBR) %>%
-    mutate(IC_95 = {
-      ci = clopper_pearson_ci(N, total, CI = "two.sided", alpha = 0.05)
+    mutate(ic_95 = {
+      ci = clopper_pearson_ci(n, total, CI = "two.sided", alpha = 0.05)
       glue("[{round(ci$Lower.limit*100, 1)};{round(ci$Upper.limit*100, 1)}]")
     },
-    .by= Name) %>%
-    add_class("ORR_table") %>%
-    add_class("show_CBR_TRUE")
-  } else {
-    summary_df = bind_rows(ORR, response_counts) %>%
-      mutate(IC_95 = {
-        ci = clopper_pearson_ci(N, total, CI = "two.sided", alpha = 0.05)
-        glue("[{round(ci$Lower.limit*100, 1)};{round(ci$Upper.limit*100, 1)}]")
-      },
-      .by= Name) %>%
-      add_class("ORR_table")%>%
-      add_class("show_CBR_FALSE")
-  }
-
-  if (confirmed){
-    summary_df = summary_df %>%
-      structure(Name="Confirmed Best Response during treatment", N=paste0("N=",total), Percentage = "%", IC_95 = "IC 95%") %>%
-      add_class("confirmed_TRUE")
-  } else {
-    summary_df = summary_df %>%
-      structure(Name="Unconfirmed Best Response during treatment", N=paste0("N=",total), Percentage = "%", IC_95 = "IC 95%") %>%
-      add_class("confirmed_FALSE")
-  }
+    .by= best_response) %>%
+    add_class("aggregate_recist_rates") %>%
+    structure(show_CBR=show_CBR, confirmed = confirmed, total = total)
 
   summary_df
 }
 
-
-#' Turns an `ORR_table` object into a formatted `flextable`
+#' Turns an `aggregate_recist_rates` object into a formatted `flextable`
 #'
-#' @param x a dataframe, resulting of `ORR_table()`
+#' @param x a dataframe, resulting of `aggregate_recist_rates()`
 #' @param ... unused
 #'
 #' @return a formatted flextable
-#' @rdname ORR_table
+#' @rdname aggregate_recist_rates
 #' @export
 #'
-#' @importFrom flextable surround delete_rows footnote as_paragraph
+#' @importFrom flextable surround delete_rows footnote as_paragraph set_header_labels
 #' @importFrom officer fp_border
-as_flextable.ORR_table = function(x, ...){
+as_flextable.aggregate_recist_rates = function(x, ...){
   check_dots_empty()
-  colnames(x) = c(attr(x,"Name"), attr(x,"N"), attr(x,"Percentage"), attr(x,"IC_95"))
+  show_CBR = attr(x, "show_CBR")
+  confirmed = attr(x, "confirmed")
+  total = attr(x,"total")
+  LABEL_CP = "Clopper-Pearson (Exact) method was used for confidence interval"
+  LABEL_CONFIRMED = "For CR & PR confirmation of response had to be be demonstrated with an assessment 4 weeks or later from the initial response for response."
+  LABEL_CBR = "CBR was defined as the presence of at least a partial response (PR), complete response (CR), or stable disease (SD) lasting at least six months (using a window of +/-1 month for the RECIST date)."
   Best_Response_during_treatment =  x %>%
     flextable() %>%
-    width(width = c(3, 1, 1, 1)) %>%
+    set_table_properties(layout="autofit") %>%
     delete_rows(i = 1, part = "footer") %>%
     bold(bold = TRUE, part = "header") %>%
     surround(i = c(1, 6), j = 1:4, border.bottom = fp_border(color = "black", style = "solid", width = 1), part = "body")
 
-    if ("confirmed_FALSE" %in% class(x)){
+    if (!confirmed){
     Best_Response_during_treatment =  Best_Response_during_treatment %>%
+      set_header_labels(best_response="Unconfirmed Best Response during treatment", n=paste0("N=",total), p = "%", ic_95 = "IC 95%") %>%
       footnote(i = 1, j = 4,
-                value = as_paragraph("Clopper-Pearson (Exact) method was used for confidence interval"),
+                value = as_paragraph(LABEL_CP),
                 ref_symbols ="*", part = "header") %>%
       valign(valign = "bottom", part = "header")
     } else{
       Best_Response_during_treatment =  Best_Response_during_treatment %>%
+      set_header_labels(best_response="Confirmed Best Response during treatment", n=paste0("N=",total), p = "%", ic_95 = "IC 95%") %>%
       footnote(i = 1, j = c(4, 1),
-                value = as_paragraph(c("Clopper-Pearson (Exact) method was used for confidence interval",
-                    "For CR & PR confirmation of response had to be be demonstrated with an assessment 4 weeks or later from the initial response for response.")),
+                value = as_paragraph(c(LABEL_CP, LABEL_CONFIRMED)),
                 ref_symbols =c("*", "1"), part = "header") %>%
       valign(valign = "bottom", part = "header")
       }
 
-  if ("show_CBR_FALSE" %in% class(x)){
+  if (!show_CBR){
     Best_Response_during_treatment =  Best_Response_during_treatment %>%
       bold(i = 1, j = c(1:4), bold = TRUE, part = "body")
-  } else if("show_CBR_TRUE" %in% class(x) & "confirmed_TRUE" %in% class(x)){
+  } else if(show_CBR & confirmed){
     Best_Response_during_treatment =  Best_Response_during_treatment %>%
       bold(i = c(1, 7), j = c(1:4), bold = TRUE, part = "body") %>%
       footnote( i = 7, j = 1,
-                value = as_paragraph("CBR was defined as the presence of at least a partial response (PR), complete response (CR), or stable disease (SD) lasting at least six months (using a window of +/-1 month for the RECIST date)."),
+                value = as_paragraph(LABEL_CBR),
                 ref_symbols = "2", part = "body") %>%
       valign(valign = "bottom", part = "header")
   } else{
     Best_Response_during_treatment =  Best_Response_during_treatment %>%
       bold(i = c(1, 7), j = c(1:4), bold = TRUE, part = "body") %>%
       footnote( i = 7, j = 1,
-                value = as_paragraph("CBR was defined as the presence of at least a partial response (PR), complete response (CR), or stable disease (SD) lasting at least six months (using a window of +/-1 month for the RECIST date)."),
+                value = as_paragraph(LABEL_CBR),
                 ref_symbols = "1", part = "body") %>%
       valign(valign = "bottom", part = "header")
   }
