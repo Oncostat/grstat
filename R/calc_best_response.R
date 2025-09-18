2ae2e6117018711564709d6279a4a143773d2295
AlexisCochard <146321117+AlexisCochard@users.noreply.github.com>
2025-09-18 16:10:55 +0200

    ajout des case_when en fonction interne

--- R/calc_best_response.R
+++ R/calc_best_response.R
@@ -108,47 +108,18 @@ calc_best_response = function(data_recist, ...,
       structure(confirmed = confirmed)
   } else {
     data_recist %>%
-      mutate(confirmed_response = case_when(
-        response_num == 1 & previous_response_num == 1 & delta_date >= cycle_length                ~ 1,
-        response_num == 1 & previous_response_num == 1 & delta_date < cycle_length                 ~ 3,
-        response_num == 1 & previous_response_num == 2 & delta_date >= cycle_length                ~ 2,
-        response_num == 1 & previous_response_num == 2 & delta_date < cycle_length                 ~ 3,
-        response_num == 1 & previous_response_num == 3                                             ~ 3,
-        response_num == 1 & previous_response_num == 5                                             ~ 5,
-
-        response_num == 2 & previous_response_num <= 2 & delta_date >= cycle_length                ~ 2,
-        response_num == 2 & previous_response_num <= 2 & delta_date < cycle_length                 ~ 3,
-        response_num == 2 & previous_response_num == 3 & previous_response_num_2 ==3               ~ 3,
-        response_num == 2 & previous_response_num == 3 & previous_response_num_2 ==5               ~ 3,
-        response_num == 2 & previous_response_num == 3 & previous_response_num_2 <= 2              ~ 2,
-        response_num == 2 & previous_response_num == 5 & previous_response_num_2 ==3               ~ 3,
-        response_num == 2 & previous_response_num == 5 & previous_response_num_2 ==5               ~ 5,
-        response_num == 2 & previous_response_num == 5 & previous_response_num_2 <= 2              ~ 2,
-
-        response_num == 4                                                                          ~ 4,
-        is.na(previous_response_num) & response_num <= 2                                           ~ 5,
-        is.na(previous_response_num_2)& (previous_response_num==3 | previous_response_num ==5) &
-          response_num <= 2                                                                        ~ 5,
-
-        TRUE ~ response_num
-      )) %>%
+      mutate(confirmed_response = .confirm_response(response_num = response_num,
+                                                    previous_response_num = previous_response_num,
+                                                    delta_date = delta_date,
+                                                    cycle_length = cycle_length,
+                                                    previous_response_num_2 = previous_response_num_2)) %>%
       mutate(bestresponse = min(confirmed_response), .by=subjid) %>%
       mutate(response_num_lead = lead(response_num),
              response_num_lead_2 = lead(response_num,2),
-             confirm = case_when(
-               response_num == 1 & bestresponse == 1 & response_num_lead ==1                              ~ 1,
-               response_num == 1 & bestresponse == 2 & response_num_lead ==2                              ~ 1,
-               response_num == 1 & bestresponse == 1 & (response_num_lead ==3 |response_num_lead ==5) &
-                 response_num_lead_2 <=2                                                                  ~ 1,
-               response_num == 2 & bestresponse == 2 & response_num_lead <=2                              ~ 1,
-               response_num == 2 & bestresponse == 2 & (response_num_lead ==3 |response_num_lead ==5) &
-                 response_num_lead_2 <=2                                                                  ~ 1,
-               response_num == 3 & bestresponse == 3                                                      ~ 1,
-               response_num == 4 & bestresponse == 4                                                      ~ 1,
-               response_num == 5 & bestresponse == 5                                                      ~ 1,
-
-               TRUE ~ 0
-             )) %>%
+             confirm = .confirm_response_for_date(response_num = response_num,
+                                                  bestresponse = bestresponse,
+                                                  response_num_lead = response_num_lead,
+                                                  response_num_lead_2 = response_num_lead_2)) %>%
       mutate(confirm = cumsum(confirm), .by=subjid) %>%
       filter(confirm==1) %>%
       slice_min(order_by =date ,by=subjid) %>%
@@ -213,4 +184,60 @@ calc_best_response = function(data_recist, ...,
     grstat_data_warn("Patients with duplicate date of recist evalution but different evaluation are present in the data set",
                      class="check_best_resp_duplic_eval_warning")
   df
+}
+
+
+#' @noRd
+#' @keywords internal
+.confirm_response = function(response_num = response_num,
+                             previous_response_num = previous_response_num,
+                             delta_date = delta_date,
+                             cycle_length = cycle_length,
+                             previous_response_num_2 = previous_response_num_2) {
+case_when(
+  response_num == 1 & previous_response_num == 1 & delta_date >= cycle_length                ~ 1,
+  response_num == 1 & previous_response_num == 1 & delta_date < cycle_length                 ~ 3,
+  response_num == 1 & previous_response_num == 2 & delta_date >= cycle_length                ~ 2,
+  response_num == 1 & previous_response_num == 2 & delta_date < cycle_length                 ~ 3,
+  response_num == 1 & previous_response_num == 3                                             ~ 3,
+  response_num == 1 & previous_response_num == 5                                             ~ 5,
+
+  response_num == 2 & previous_response_num <= 2 & delta_date >= cycle_length                ~ 2,
+  response_num == 2 & previous_response_num <= 2 & delta_date < cycle_length                 ~ 3,
+  response_num == 2 & previous_response_num == 3 & previous_response_num_2 ==3               ~ 3,
+  response_num == 2 & previous_response_num == 3 & previous_response_num_2 ==5               ~ 3,
+  response_num == 2 & previous_response_num == 3 & previous_response_num_2 <= 2              ~ 2,
+  response_num == 2 & previous_response_num == 5 & previous_response_num_2 ==3               ~ 3,
+  response_num == 2 & previous_response_num == 5 & previous_response_num_2 ==5               ~ 5,
+  response_num == 2 & previous_response_num == 5 & previous_response_num_2 <= 2              ~ 2,
+
+  response_num == 4                                                                          ~ 4,
+  is.na(previous_response_num) & response_num <= 2                                           ~ 5,
+  is.na(previous_response_num_2)& (previous_response_num==3 | previous_response_num ==5) &
+    response_num <= 2                                                                        ~ 5,
+
+  TRUE ~ response_num
+)
+}
+
+#' @noRd
+#' @keywords internal
+.confirm_response_for_date = function(response_num = response_num,
+                                      bestresponse = bestresponse,
+                                      response_num_lead = response_num_lead,
+                                      response_num_lead_2 = response_num_lead_2) {
+  case_when(
+    response_num == 1 & bestresponse == 1 & response_num_lead ==1                              ~ 1,
+    response_num == 1 & bestresponse == 2 & response_num_lead ==2                              ~ 1,
+    response_num == 1 & bestresponse == 1 & (response_num_lead ==3 |response_num_lead ==5) &
+      response_num_lead_2 <=2                                                                  ~ 1,
+    response_num == 2 & bestresponse == 2 & response_num_lead <=2                              ~ 1,
+    response_num == 2 & bestresponse == 2 & (response_num_lead ==3 |response_num_lead ==5) &
+      response_num_lead_2 <=2                                                                  ~ 1,
+    response_num == 3 & bestresponse == 3                                                      ~ 1,
+    response_num == 4 & bestresponse == 4                                                      ~ 1,
+    response_num == 5 & bestresponse == 5                                                      ~ 1,
+
+    TRUE ~ 0
+  )
 }
