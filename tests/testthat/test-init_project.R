7d8af12c832963fa9cc99b4a6d56ed2219c11293
Dan Chaltiel <15105152+DanChaltiel@users.noreply.github.com>
2025-09-23 17:53:40 +0200

    update test

--- tests/testthat/test-init_project.R
+++ tests/testthat/test-init_project.R
@@ -5,8 +5,9 @@ test_that("gr_new_project works", {
   path = fs::path_temp("/test/test_init_project_dir")
   unlink(path, recursive=TRUE)
 
-  gr_new_project(path, open=FALSE, verbose=FALSE)
+  gr_new_project(path, open=FALSE, verbose=FALSE, trial_name="TEST", headers=NULL)
 
+  # browseURL(path)
   # fs::dir_tree(path)
 
   copied_files = dir_ls(path, type="file", recurse=TRUE)
@@ -16,7 +17,7 @@ test_that("gr_new_project works", {
   expect_equal(length(copied_files), length(pkg_files))
 
 
-  gr_new_project(path, open=FALSE, verbose=FALSE) %>%
+  gr_new_project(path, open=FALSE, verbose=FALSE, trial_name="TEST", headers=NULL) %>%
     expect_error(class="gr_new_project_notempty_error")
 
   unlink(path, recursive=TRUE)
