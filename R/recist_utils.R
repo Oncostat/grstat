470d1b466bd04b251792b57ac6b16bad88b80d4a
Dan Chaltiel <dan.chaltiel@gmail.com>
2025-10-06 17:22:44 +0200

    fix `.recist_to_num()` turning "Non CR non PD" to "CR" (#91)
    * move .encode_response to recist_utils.R
    
    * fix .recist_to_num()
    
    Non-CR was detected as CR
    
    * replace calls to .encode_response by .recist_to_num
    
    seems to make no difference
    
    * .recist_from_num with NTL labels

--- R/recist_utils.R
+++ R/recist_utils.R
@@ -8,10 +8,10 @@
 #' @importFrom stringr str_detect
 .recist_to_num = function(x){
   rtn = case_when(
+    str_detect(x, "(?i)non.*cr.*pd")   ~ 3,
     str_detect(x, "(?i)(\\W|^)(CR)(\\W|$)") | str_detect(x, "(?i)complete") ~ 1,
     str_detect(x, "(?i)(\\W|^)(PR)(\\W|$)") | str_detect(x, "(?i)partial")  ~ 2,
     str_detect(x, "(?i)(\\W|^)(SD)(\\W|$)") | str_detect(x, "(?i)stable")   ~ 3,
-    str_detect(x, "(?i)non.*cr.*pd")   ~ 3,
     str_detect(x, "(?i)(\\W|^)(PD)(\\W|$)") | str_detect(x, "(?i)progres")  ~ 4,
     is.na(x) | x %in% c("NE", "NA") | str_detect(x, "(?i)not [eval|avai]")  ~ 5,
     .default=Inf,
@@ -31,10 +31,15 @@
 #' Encode numeric RECIST response as standard acronyms
 #' Non-CR Non-PD are turned to SD for simplicity
 #' @noRd
-.recist_from_num = function(x) {
+.recist_from_num = function(x, non_target=FALSE) {
   assert(is.numeric(x))
-  x[x==99] = 5
-  factor(x, levels=c(1:5), labels=c("CR", "PR", "SD", "PD", "Not evaluable"))
+  x[is.infinite(x)] = 5
+  if(isTRUE(non_target)){
+    labels=c("CR", "Non-CR Non-PD", "Non-CR Non-PD", "PD", "Not evaluable")
+  } else {
+    labels=c("CR", "PR", "SD", "PD", "Not evaluable")
+  }
+  factor(x, levels=c(1:5), labels=labels)
 }
 
 
