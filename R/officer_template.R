b04c814ab83c17f10880046d5a971b39f1cddadc
CHALTIEL Dan <dan.chaltiel@gmail.com>
2026-02-12 11:26:24 +0100

    removed doc_properties

--- R/officer_template.R
+++ R/officer_template.R
@@ -92,11 +92,15 @@ gr_officer_template = function(
       DATE_FREEZE=date_freeze
     ) %>%
     officer::footers_replace_text_at_bkm("TRIAL_NAME_FOOTER", acronym) %>%
-    body_replace_flextable_at_bkm("TRIAL_AUTHORS",
-                                  .flextable_authors(authors, title="REPORT AUTHORS")) %>%
-    body_replace_flextable_at_bkm("TRIAL_SPONSOR",
-                                  .flextable_authors(sponsor, title="SPONSOR")) %>%
+    officer::body_replace_flextable_at_bkm("TRIAL_AUTHORS",
+                                           .flextable_authors(authors, title="REPORT AUTHORS")) %>%
+    officer::body_replace_flextable_at_bkm("TRIAL_SPONSOR",
+                                           .flextable_authors(sponsor, title="SPONSOR")) %>%
     officer::cursor_end()
+
+  rtn$doc_properties$data["creator", "value"] = "Bureau de Biostatistique et d’Épidémiologie"
+  rtn$doc_properties$data["lastModifiedBy", "value"] = ""
+
   rtn
 }
 
