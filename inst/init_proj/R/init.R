a9bc835365c45f26f01da1fedfc212d307ef0614
Dan Chaltiel <dan.chaltiel@gmail.com>
2026-03-20 14:27:29 +0100

    Fix `gr_new_project()` (#89)
    * new `body_add_gr_sections_1_8()`
    
    * update project internal files
    
    * improved `gr_new_project()`
    
    * remove shiny attempt + clean
    
    * update test
    
    * doc
    
    * move crosstable to imports
    
    * minor fix for check
    
    * fix: review de Anne
    
    * fix: gr_new_project_notempty_error
    
    * fix: review Anne + Ikram
    
    ---------
    
    Co-authored-by: Dan Chaltiel <15105152+DanChaltiel@users.noreply.github.com>

--- inst/init_proj/R/init.R
+++ inst/init_proj/R/init.R
@@ -1,20 +1,25 @@
+
+
 # Global options ----------------------------------------------------------
 
-#Si installation des packages bloquée par le firewall, décommenter la ligne suivante
-# Sys.setenv(R_LIBCURL_SSL_REVOKE_BEST_EFFORT=TRUE) 
+#Si l'installation des packages est bloquée par le firewall, décommenter la ligne suivante :
+# Sys.setenv(R_LIBCURL_SSL_REVOKE_BEST_EFFORT=TRUE)
 
 
 Sys.setenv(LANGUAGE="en")
-Sys.setlocale("LC_TIME", "English")
+Sys.setlocale("LC_TIME", "C")
 options(
-  repos="http://cran.rstudio.com/",
-  encoding="UTF-8", 
-  warn=1, 
-  warnPartialMatchArgs=TRUE, 
-  stringsAsFactors=FALSE, 
-  dplyr.summarise.inform=FALSE,
-  lifecycle_verbosity="warning",
-  tidyverse.quiet=TRUE
+  repos = "https://cran.rstudio.com/",
+  encoding = "UTF-8",
+  warn = 1,
+  warnPartialMatchArgs = TRUE,
+  warnPartialMatchDollar = TRUE,
+  warnPartialMatchAttr = TRUE,
+  rlang_backtrace_on_error = "full",
+  dplyr.summarise.inform = FALSE,
+  lubridate.week.start = 1,
+  lifecycle_verbosity = "warning",
+  tidyverse.quiet = TRUE
 )
 
 globalCallingHandlers(NULL)
@@ -26,18 +31,23 @@ rlang::global_entrace()
 suppressPackageStartupMessages(suppressWarnings({
   library(tidyverse)
   library(glue)
+  library(cli)
   library(officer)
   library(flextable, exclude="compose")
   library(scales, exclude=c("discard", "col_factor"))
   library(patchwork)
-  
+
   library(crosstable, exclude="compact")
   library(EDCimport)
+  library(grstat)
+
+  library(survival)
   library(ggsurvfit)
 }))
 
-stopifnot(packageVersion("crosstable")>="0.7.0.9000")
-stopifnot(packageVersion("EDCimport")>="0.4.1.9029")
+stopifnot(packageVersion("crosstable")>="VAR_CROSSTABLE_VERSION")
+stopifnot(packageVersion("EDCimport")>="VAR_EDCIMPORT_VERSION")
+stopifnot(packageVersion("grstat")>="VAR_GRSTAT_VERSION")
 
 edc_inform_code()
 
@@ -47,9 +57,10 @@ options(
   ggplot2.discrete.colour=ggsci::scale_colour_lancet,
   ggplot2.discrete.fill=ggsci::scale_fill_lancet
 )
+theme_set(theme_light())
 
 crosstable_options(
-  zero_percent=TRUE,
+  remove_zero_percent=TRUE,
   percent_digits=0,
   margin="cols",
   padding_v=0,
@@ -66,24 +77,49 @@ set_flextable_defaults(big.mark="")
 
 # Custom fonctions ---------------------------------------------------------------
 
-proto = function() browseURL("../Documents/protocole.pdf")
+#FIXME Changez les chemins d'accès aux documents de l'étude pour pouvoir utiliser `proto()` dans
+# la console et ouvrir le protocole sans avoir à connaître le chemin d'accès exact à chaque fois.
+# Utilisez des chemins d'accès absolus ou relatifs, comme vous préférez.
+# Pour un chemin absolu vers le NAS, il faut utiliser `normalizePath()`.
+
 crf = function() browseURL("../Documents/crf.pdf")
-feuille_dm = function() browseURL("https://link/to/nextcloud")
+proto = function() browseURL("../Documents/protocole.pdf")
+# proto = function() browseURL(normalizePath("//nas-01/SBE_ETUDES/path/to/protocole.pdf"))
 wd = function() browseURL(".")
 
-v=View
+
+#Helper pour versionnaliser tous les graphiques
+DEFAULT_FOLDER = glue("graph/{date_extraction}/")
+dir.create(folder, showWarnings=FALSE)
+ggsave_here = function(filename, folder = DEFAULT_FOLDER, ...){
+  ggsave(glue("{folder}{filename}"), ...)
+}
 
 
-inf_na = function(x) ifelse(is.infinite(x), NA, x)
-sumna = function(...) sum(..., na.rm=TRUE)
-max_na = function(x, na.rm=TRUE) if(all(is.na(x))) NA_real_ else max(x, na.rm=na.rm)
-min_na = function(x, na.rm=TRUE) if(all(is.na(x))) NA_real_ else min(x, na.rm=na.rm)
+inf_narm = function(x) ifelse(is.infinite(x), NA, x)
+sum_narm = function(...) sum(..., na.rm=TRUE)
+max_narm = function(x) if(all(is.na(x))) NA_real_ else max(x, na.rm=TRUE)
+min_narm = function(x) if(all(is.na(x))) NA_real_ else min(x, na.rm=TRUE)
 date_minf = structure(-Inf, class = "Date")
 date_inf = structure(Inf, class = "Date")
 
 ggplotly = plotly::ggplotly
+v=View
 
 `%0%` = function (x, y) if(length(x)==0) y else x
+
+glue_vec = function(...){
+  map_chr(c(...), cli::format_inline)
+}
+
+pivot_longer_lab = function(data, cols, transform=identity, ...) {
+  transform = rlang::as_function(transform)
+  nms = data %>% select({{cols}}) %>% names()
+  labs = data %>% select({{cols}}) %>% map(~attr(.x, "label")) %>%
+    transform() %>% set_names(nms)
+  pivot_longer(data, {{cols}}, ...) %>%
+    mutate(name=labs[name])
+}
 
 midpoint = function(starttime, stoptime){
   as.Date((as.numeric(stoptime) + as.numeric(starttime)) / 2, origin = '1970-01-01')
@@ -91,15 +127,15 @@ midpoint = function(starttime, stoptime){
 
 flextable2 = function(x, autofit=TRUE, fontsize_body=8, fontsize_head=9, padding_v=0){
   x = flextable(x)
-  
+
   if (missing(fontsize_body)) fontsize_body = getOption("crosstable_fontsize_body", fontsize_body)
   if (missing(fontsize_head)) fontsize_head = getOption("crosstable_fontsize_body", fontsize_head)
   if (missing(padding_v)) padding_v = getOption("crosstable_padding_v", padding_v)
-  
+
   if(autofit) x = set_table_properties(x, layout="autofit")
-  x %>% 
-    fontsize(size=fontsize_body) %>% 
-    fontsize(size=fontsize_head, part="head") %>% 
+  x %>%
+    fontsize(size=fontsize_body) %>%
+    fontsize(size=fontsize_head, part="head") %>%
     padding(padding.top=padding_v, padding.bottom=padding_v)
 }
 
