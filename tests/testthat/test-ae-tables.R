a83db10966f391e84bbaaf68d5919aae8309f22a
Dan Chaltiel <15105152+DanChaltiel@users.noreply.github.com>
2026-06-02 21:40:49 +0200

    add tests for ae_groups

--- tests/testthat/test-ae-tables.R
+++ tests/testthat/test-ae-tables.R
@@ -56,10 +56,10 @@ test_that("ae_table_grade() with missing and grade>2", {
 
 
 test_that("ae_table_grade() with old argument names", {
-  
+
   a = ae_table_grade(db_test$ae, data_pat=db_test$enrolres, measure ="sup", percent_pattern = "{n} ({p}%)", percent_digits=2)
   b = ae_table_grade(df_ae=db_test$ae, df_enrol=db_test$enrolres, variant="sup", percent=TRUE, digits=2)
-  
+
   expect_identical(a, b)
 })
 
@@ -142,4 +142,59 @@ test_that("ae_table_soc() default snapshot", {
   ae_table_soc(df_ae=ae, df_enrol=enrolres,
                arm="ARsM", term="AETEeRM", soc="AEtSOC", grade="AEGeR", subjid="SUBaJID") %>%
     expect_error(class="grstat_name_notfound_error")
+})
+
+
+test_that("ae_table_soc(ae_groups) works", {
+
+  tm = grstat_example(p_na=0.10)
+  attach(tm, warn.conflicts = FALSE)
+
+
+  x0 = ae_table_soc(df_ae=ae, df_enrol=enrolres)
+  expect_named(x0, c("soc", "all_patients__g1", "all_patients__g2", "all_patients__g3",
+                     "all_patients__g4", "all_patients__g5", "all_patients__na",
+                     "all_patients__tot"))
+  x0 %>% as_flextable()
+
+  #default is to assign all grades to its own group
+  x1 = ae_table_soc(df_ae=ae, df_enrol=enrolres,
+                    ae_groups=list("G1"=1, "G2"=2, "G3"=3, "G4"=4, "G5"=5, "NA"=NA))
+  expect_identical(x0, x1)
+  x1 %>%
+    mutate(across(starts_with("all_patients"),
+                  ~as.numeric(str_remove(.x, " \\(.*"))),
+           tot=rowSums(across(starts_with("all_patients__g")))) %>%
+    filter(tot+all_patients__na!=all_patients__tot) %>%
+    expect_shape(nrow=0)
+
+
+  #3 groups
+  ae_groups=list("Any grade"=c(1:5,NA), "Grade 1-2"=1:2, "Grade 3-5"=3:5)
+  x2 = ae_table_soc(df_ae=ae, df_enrol=enrolres,
+                    ae_groups=ae_groups)
+  expect_named(x2, c("soc", "all_patients__any_grade", "all_patients__grade_1_2",
+                     "all_patients__grade_3_5", "all_patients__tot"))
+  expect_equal(x2$all_patients__any_grade, x2$all_patients__tot, ignore_attr=TRUE)
+  # expect_equal(x2$all_patients_any_grade,
+  #              x2$all_patients_grade_1_2+x2$all_patients_grade_3_5)
+
+
+  #3 groups with ARM
+  ae_groups=list("Any grade"=1:5, "Grade 1-2"=1:2, "Grade 3-5"=3:5)
+  x3 = ae_table_soc(df_ae=ae, df_enrol=enrolres, arm="ARM", total=FALSE,
+                    ae_groups=ae_groups)
+  expect_named(x3, c("soc",
+                     "control__any_grade", "control__grade_1_2", "control__grade_3_5",
+                     "treatment__any_grade", "treatment__grade_1_2", "treatment__grade_3_5"))
+
+
+  #errors
+  ae_table_soc(df_ae=ae, df_enrol=enrolres,
+               ae_groups="Coucou") %>%
+    expect_error(class="ae_table_soc_group_bad_class")
+  ae_table_soc(df_ae=ae, df_enrol=enrolres,
+               ae_groups=list("Grade 1-2"=0:2, "Grade sup 3"=3:7)) %>%
+    expect_error(class="ae_table_soc_group_bad_number")
+
 })
